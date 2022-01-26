9728cea0d2c0f54e07bd97f9f8f5c927e48ede38
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-01-26 19:57:18 +0000

    Install create-react-app, delete some assets

---
+++ motorwash-client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      header
+      {/* nav/header */}
+      {/* Home */}
+      {/* Services */}
+      {/* Who we are */}
+      {/* Pricing */}
+      {/* Gallery */}
+      {/* Contact */}
+      {/* Register */}
+      {/* Login */}
+    </div>
+  );
+}
+
+export default App;
