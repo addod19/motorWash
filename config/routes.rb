db8b60f6fa3fe8d0c2bdf0c3bdd724b434852875
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-02-07 18:05:51 +0000

    Add resources for bookings routing for create and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
+  resources :bookings, only: [:create, :show]
   post '/login', to: 'users#login'
   get 'auto_login', to: 'users#auto_login'
   delete '/logout', to: 'users#logout'
