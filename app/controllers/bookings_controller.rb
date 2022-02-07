cb38048e66da8653657e174d6c4632c7f1b961fa
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-02-07 18:11:36 +0000

    Update controller action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,29 @@
 class BookingsController < ApplicationController
+
+  def create
+    @booking = Bookings.new(bookings_params)
+
+    if @booking.save!
+      render json: { booking: @booking, msg: "Booking created" }
+    else
+      render json: { error: "Booking could not be created!!! Oooooopps" }
+    end
+  end
+
+  def show
+    @booking = Bookings.find_by(bookings_id)
+    
+    if @booking
+      render json: { booking: @booking, msg: "Success" }
+    else
+      render json: { error: "Error displaying booking" }
+    end
+  end
+
+  private
+
+  def bookings_params
+    params.require(:bookingDate, :bookingTime, :user_id)
+  end
+
 end
